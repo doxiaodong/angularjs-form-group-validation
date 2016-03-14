5b205e35e4e8e8a1b595749f6ec499eb592a525e
duxiaodong <duxiaodong@darlin.me>
2016-03-14 19:30:00 +0800

    format

--- src/form-group-item.directive.js
+++ src/form-group-item.directive.js
@@ -1,40 +1,40 @@
-(function(angular) {
+(function (angular) {
 
-	angular.module('formGroup.validation.service', [])
+    angular.module('formGroup.validation.service', [])
 
-	.directive('formGroupItem', ['formGroupValidationService', function(formGroupValidationService) {
-		return {
-			restrict: 'A',
-	    require: ['^formGroupValidation', '?ngModel'],
-	    link: function(scope, element, attrs, ctrls) {
+        .directive('formGroupItem', ['formGroupValidationService', function (formGroupValidationService) {
+            return {
+                restrict: 'A',
+                require: ['^formGroupValidation', '?ngModel'],
+                link: function (scope, element, attrs, ctrls) {
 
-	    	var formGroupValidationCtrl = ctrls[0];
-	    	var ngModelCtrl = ctrls[1];
+                    var formGroupValidationCtrl = ctrls[0];
+                    var ngModelCtrl = ctrls[1];
 
-	    	if (!formGroupValidationCtrl.init) {
-	    		return;
-	    	}
+                    if (!formGroupValidationCtrl.init) {
+                        return;
+                    }
 
-	    	var init = formGroupValidationCtrl.init();
+                    var init = formGroupValidationCtrl.init();
 
-	    	var _form = init.form;
-	    	var _formGroup = init.formGroup;
-	    	_formGroup.list.push(ngModelCtrl);
+                    var _form = init.form;
+                    var _formGroup = init.formGroup;
+                    _formGroup.list.push(ngModelCtrl);
 
-	    	var formGroupNg = _form[_formGroup.name];
+                    var formGroupNg = _form[_formGroup.name];
 
-	    	ngModelCtrl.$validators.formGroupItem = function(modelValue, viewValue) {
-	    		validationChecked = formGroupValidationService.validationCheck(_formGroup.list);
-	    		formGroupNg.$valid = !validationChecked.invalid;
-	    		formGroupNg.$invalid = validationChecked.invalid;
+                    ngModelCtrl.$validators.formGroupItem = function (modelValue, viewValue) {
+                        validationChecked = formGroupValidationService.validationCheck(_formGroup.list);
+                        formGroupNg.$valid = !validationChecked.invalid;
+                        formGroupNg.$invalid = validationChecked.invalid;
 
-	    		formGroupNg.$dirty = validationChecked.dirty;
+                        formGroupNg.$dirty = validationChecked.dirty;
 
-	    		return true; // always return true to ensure no effect by formGroupItem
-	    	};
+                        return true; // always return true to ensure no effect by formGroupItem
+                    };
 
-	    }
-		};
-	}]);
+                }
+            };
+        }]);
 
 })(angular);
--- src/form-group-validation.directive.js
+++ src/form-group-validation.directive.js
@@ -1,34 +1,34 @@
-(function(angular) {
+(function (angular) {
 
-	angular.module('formGroup.validation', ['formGroup.validation.service', 'formGroup.item'])
+    angular.module('formGroup.validation', ['formGroup.validation.service', 'formGroup.item'])
 
-	.directive('formGroupValidation', function() {
-		return {
-			restrict: 'AE',
-	    scope: {
-	    	_formGroupForm: '=formGroupValidation'
-	    },
-	    controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
+        .directive('formGroupValidation', function () {
+            return {
+                restrict: 'AE',
+                scope: {
+                    _formGroupForm: '=formGroupValidation'
+                },
+                controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
 
-	    	_formGroup = {
-	    		name: $attrs.name,
-	    		list: []
-	    	};
+                    _formGroup = {
+                        name: $attrs.name,
+                        list: []
+                    };
 
-	    	if (!_formGroup.name) {
-	    		return;
-	    	}
+                    if (!_formGroup.name) {
+                        return;
+                    }
 
-	    	$scope._formGroupForm[_formGroup.name] = {};
+                    $scope._formGroupForm[_formGroup.name] = {};
 
-	    	this.init = function() {
-	    		return {
-	    			form: $scope._formGroupForm,
-	    			formGroup: _formGroup
-	    		};
-	    	};
-		  }]
-		};
-	});
+                    this.init = function () {
+                        return {
+                            form: $scope._formGroupForm,
+                            formGroup: _formGroup
+                        };
+                    };
+                }]
+            };
+        });
 
 })(angular);
